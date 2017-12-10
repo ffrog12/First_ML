eb812a45bf44d78426b07d2045b0c780b88b8c34
Fred Isaac <ffrog12@gmail.com>
2017-12-10 11:29:38 +0000

    adding SVM to the program

--- First_ML/First_Program/demo1.py
+++ First_ML/First_Program/demo1.py
@@ -1,7 +1,7 @@
 from sklearn import tree
+from sklearn import svm
 
 #height, weight, shoe size
-
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
@@ -9,10 +9,27 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 'female', 'male', 'male']
 
-clf = tree.DecisionTreeClassifier()
+#Data that prediction will run on
+testdata = [192,92,45]
 
-clf = clf.fit(X,Y)
+# Tree Classifier
+clftree = tree.DecisionTreeClassifier()
 
-prediction = clf.predict([[190,70,43]])
+clftree = clftree.fit(X,Y)
 
-print(prediction)
+# SVM Classifier
+clfsvm = svm.SVC()
+
+clfsvm = clfsvm.fit(X,Y)
+
+#Predictions of Different Classifiers
+predictiontree = clftree.predict([testdata])
+
+predictionsvm = clfsvm.predict([testdata])
+
+#Print of best prediction
+print('Tree Prediction')
+print(predictiontree)
+
+print('svm Prediction')
+print(predictionsvm)
