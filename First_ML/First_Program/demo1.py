59f346e4540da91229887418ca73ad5d638ecf18
Fred Isaac <ffrog12@gmail.com>
2017-12-10 19:16:08 +0000

    adding prediction of algo quality and trying to print only the best(but had to comment it out

--- First_ML/First_Program/demo1.py
+++ First_ML/First_Program/demo1.py
@@ -1,33 +1,34 @@
 from sklearn import tree
 from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 #height, weight, shoe size
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+Xtrain = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+Ytrain = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 'female', 'male', 'male']
 
 #Data that prediction will run on
-testdata = [160,59,40]
+testdata = [192,92,47]
 
 # Tree Classifier
 clftree = tree.DecisionTreeClassifier()
 
-clftree = clftree.fit(X,Y)
+clftree = clftree.fit(Xtrain,Ytrain)
 
 # SVM Classifier
 clfsvm = svm.SVC()
 
-clfsvm = clfsvm.fit(X,Y)
+clfsvm = clfsvm.fit(Xtrain,Ytrain)
 
 #Gaussian Naive Bayesian Classifier
 
 clfgnb = GaussianNB()
 
-clfgnb = clfgnb.fit(X,Y)
+clfgnb = clfgnb.fit(Xtrain,Ytrain)
 
 #Predictions of Different Classifiers
 predictiontree = clftree.predict([testdata])
@@ -36,12 +37,45 @@ predictionsvm = clfsvm.predict([testdata])
 
 predictiongnb = clfgnb.predict([testdata])
 
+#Test which prediction is best
+
+#validation data
+Xtest = [[190, 70, 43], [150, 70, 30], [170, 80, 38], [200, 90, 44], [160, 50, 32]]
+
+Ytest = ['male', 'female', 'male', 'male', 'female']
+
+#Trialling against validation data
+testpredtree = clftree.predict(Xtest)
+testpredsvm = clfsvm.predict(Xtest)
+testpredgnb = clfgnb.predict(Xtest)
+
+#Scoring against validation
+treeacc = accuracy_score(testpredtree, Ytest)
+svmacc = accuracy_score(testpredsvm, Ytest)
+gnbacc = accuracy_score(testpredgnb, Ytest)
+
 #Print of best prediction
-print('Tree Prediction')
-print(predictiontree)
 
+#if svmacc >= treeacc or gnbacc:
+#   pass:
 print('SVM Prediction')
-print(predictionsvm)
+print (predictionsvm)
+print (", accuracy score")
+print (svmacc * 100)
+print('%')
 
-print('Gaussian Niave Bayes Prediction')
-print(predictiongnb)
+#elif treeacc > gnbacc:
+#   pass:
+print('Tree Prediction')
+print (predictiontree)
+print (", accuracy score")
+print (treeacc * 100)
+print('%')
+
+#else:
+#   pass:
+print('GNB Prediction')
+print (predictiongnb)
+print (", accuracy score")
+print (gnbacc * 100)
+print('%')
